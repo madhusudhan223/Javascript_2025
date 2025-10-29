ee6498cbb548031362a2614413a8db6f7fd015e0
Madhu sudhan <madhusudhan.kalle@landmarkgroup.in>
2025-10-29 09:22:58 +0530

    added js files

--- practice.js
+++ practice.js
@@ -2,3 +2,10 @@
 
 console.log(1+2);
 console.log("hello world");
+
+
+
+const symbol1 = Symbol("Test");
+const symbol2 = Symbol("Test");
+
+console.log(symbol1 == symbol2)
--- variables.js
+++ variables.js
@@ -14,7 +14,7 @@
 let personName = null;
 
 // personName = "raj";
-console.log(typeof personName)
+// console.log(typeof personName)
 
 let age = 25;
 
@@ -54,6 +54,141 @@ let age = 25;
 // objects
 // arrays
 // functions
+
+
+// objects --> 
+
+// in java --> name, id:
+
+// in js --> objects will be key value pairs
+
+
+const obj = {};
+const person = {
+    name: "Raj",
+    id: 223,
+    address:{
+        houseno: "1-28",
+        street:{
+
+        }
+    },
+    details: []
+}
+
+
+const obj1 = {name: "raj"} // sjhdfghsdgf
+const obj2 = {name: "raj"} // reterterter
+
+// console.log(obj1 == obj2)
+
+
+const name1 = "raj"
+const name2 = "raj"
+
+const number1 = 223;
+const number2 = 223;
+
+// console.log(number1 == number2)
+
+// console.log(name1 == name2)
+
+// two ways to access objects
+
+// dot notation 
+// bracket notation
+
+// js operators -- 
+
+// console.log(person.address.houseno)
+
+
+// 
+
+const response = 'address'
+
+// console.log(person[response]['houseno'])
+
+// console.log(person['name']); // bracket notation
+// console.log(person.name) // dot notation
+
+
+
+// arrays
+
+const arr = [];
+
+const numbers = [1, 2, 3, 4, "raj", {}];
+const names = ["raj", "hello"];
+const userDetails = [{ name: "raj" }, { name: "ram" }, { name: "ravi" }]
+
+
+// console.log(userDetails[3]);
+// console.log(names[0])
+
+
+
+
+
+
+
+// arithmentc -->  + - * %, / --> 
+
+// assignment operator --> +=, -=, =
+
+// comparision operators --> ==, <=, < , >=, >, !=
+
+// logical operator --> &&, ||, &, |
+
+// !=, !==
+
+// ==, ===
+
+console.log(2 == 2);
+console.log( 2 == '2')  //it check for only values are same or not. it doesnt compare data type
+console.log(2 === '2') // it checks for both value and data type
+console.log(1 != '1' );   1 != 1 
+console.log(1 !== '1'); 1 != '1'
+
+// console.log( 2 + 2)
+// console.log( 2 + '2');  // '2' + '2'
+// console.log("cat" + 5)
+
+
+let bar = 5;
+
+bar -=  15; // bar = bar + 15
+
+// bar = bar - 15, 5-15
+
+
+
+// console.log(bar)
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("hello" + " " + "world")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
